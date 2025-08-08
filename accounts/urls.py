605974e6e356881aa520e9e2fa28913d8c3d2e56
Nyasha Nallen Tumba <nyashatumba@gmail.com>
2025-08-08 12:26:32 +0200

    6.1 Password Change
    Password change for logged in users

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', views.register_syst_user, name='register'),
@@ -7,4 +8,14 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('profile/update/', views.update_user_profile, name='update_user_profile'),
     path('profile/', views.view_user_profile, name='view_user_profile'),
+
+    #PASSWORD CHANGE URLS
+    path('password-change/', auth_views.PasswordChangeView.as_view(
+        template_name='accounts/password_change.html',
+        success_url='/accounts/password-change-done/'
+    ), name='password_change'),
+
+    path('password-change-done/', auth_views.PasswordChangeDoneView.as_view(
+        template_name='accounts/password_change_done.html'
+    ), name='password_change_done'),
 ]
